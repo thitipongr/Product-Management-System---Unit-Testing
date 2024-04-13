d13983d62f564e04c7c22e341f282771f96a5e1d
thitipongr <thitipong.r.psn@gmail.com>
2024-04-13 22:39:45 +0700

    Test: Create new product failure case

--- test/products.spec.js
+++ test/products.spec.js
@@ -7,12 +7,62 @@ describe("Products", () => {
     });
 
     it("gets products by id", async () => {
-      return request
-        .get("/products/3")
-        .expect(200)
-        .then((response) => {
-          expect(response.body[0].name).toEqual("req.body.name");
-        });
+      const response = await request.get("/products/3").expect(200);
+      expect(response.body[0].name).toEqual("req.body.name");
+    });
+  });
+
+  describe("Create Products", () => {
+    it("create new products failure: empty name", async () => {
+      const response = await request
+        .post("/products")
+        .send({
+          name: "",
+          category: "testProductCategory",
+          price: 99,
+          stock: "20",
+        })
+        .expect(200);
+      expect(response.text).toEqual("invalid body");
+    });
+
+    it("create new products failure: empty price", async () => {
+      const response = await request
+        .post("/products")
+        .send({
+          name: "testProductName",
+          category: "testProductCategory",
+          price: "",
+          stock: "20",
+        })
+        .expect(200);
+      expect(response.text).toEqual("invalid body");
+    });
+
+    it("create new products failure: NaN price", async () => {
+      const response = await request
+        .post("/products")
+        .send({
+          name: "testProductName",
+          category: "testProductCategory",
+          price: "testProductPrice",
+          stock: "20",
+        })
+        .expect(200);
+      expect(response.text).toEqual("invalid body");
+    });
+
+    it("create new products failure: NaN stock", async () => {
+      const response = await request
+        .post("/products")
+        .send({
+          name: "testProductName",
+          category: "testProductCategory",
+          price: 99,
+          stock: "testProductStock",
+        })
+        .expect(200);
+      expect(response.text).toEqual("invalid body");
     });
   });
 });
